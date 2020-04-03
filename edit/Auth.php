7bb703135de2b83e262b2bc6a2016e3581b76137
gaoxiaoss <63092009+gaoxiaoss@users.noreply.github.com>
2020-04-03 15:01:02 +0800

    Add files via upload

---
+++ edit/Auth.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * 权限管理
+ */
+class Ext_Auth {
+	
+	const CONTENT_EDIT= 1;
+	
+	
+	const CATE_EDIT = 2;
+
+	
+	const WEB_EDIT = 4;
+	
+	
+	const SYS_EDIT = 8;
+	
+	
+	public static $pres = array(
+		'1' => '内容编辑',
+		'3' => '栏目编辑',
+		'7' => '整站编辑',
+		'15' => '管理员'
+	);
+
+	
+	
+	public function getPre($arg) {
+		$args = func_get_args();
+		$pre = 0;
+		foreach ($args as $value) {
+			$pre |= $value;
+		}
+		return $pre;	
+	}
+	
+	
+	public function check($havePre, $needPre) {
+		return $havePre & $needPre;	
+	}
+}
